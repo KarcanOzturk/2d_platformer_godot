0dc7b632159979cef0e60f8e546fd4e9a60367b7
Karcan Ozan Öztürk <karcanozturk1@gmail.com>
2025-02-08 01:41:49 +0300

    bg, additional tiles
    background and additional tile added

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var player: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
